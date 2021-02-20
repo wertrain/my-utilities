95faf256fe61f1a583bdfec60bc7a22d068d1a33
wertrain <wertrain@gmail.com>
2021-02-20 10:11:09 +0900

    update utilities

---
+++ venv_create_or_activate.bat
@@ -0,0 +1,19 @@
+::---------------------------------------------------------
+:: [python] 仮想環境を を作成するか activate する
+::---------------------------------------------------------
+echo off
+setlocal
+cd /d %~dp0
+:: 仮想環境名
+set VENV_NAME= .venv
+
+if not exist %VENV_NAME% goto CREATEVENV
+goto ACTIVATE
+
+:CREATEVENV
+py -m venv %VENV_NAME%
+
+:ACTIVATE
+cmd /k .venv\Scripts\activate.bat
+
+endlocal
